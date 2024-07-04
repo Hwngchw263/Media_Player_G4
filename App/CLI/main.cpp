c35c013449cfac8100d65b7b987cb91c0d123e5a
annhtuan <tuananhbka123@gmail.com>
2024-07-04 23:49:06 +0700

    them thread

--- App/CLI/main.cpp
+++ App/CLI/main.cpp
@@ -14,12 +14,50 @@ int main() {
     //controller.handleInput('h');  // Show help initially
 
     std::string input;
-    while (true) {
-        std::cout << "Enter command: ";
-        char cmd;
-        std::cin >> cmd;
-        controller.handleInput(cmd);
-    }
+    // while (true) {
+    //     std::cout << "Enter command: ";
+    //     char cmd;
+    //     std::cin >> cmd;
+    //     controller.handleInput(cmd);
+    // }
+    char cmd;
+    std::thread input_thread([&]() {
+        while (true) {
+            std::cout << "Enter command: ";
+            //EnterCharInput(command);
+                std::cin >> cmd;
+            {
+                std::lock_guard<std::mutex> lock(controller.command_mutex);
+                if (!controller.serial_command_received) {
+                    controller.handleInput(cmd);
+                    //handleHomeTabCommand(command,page_num); // Handle HomeTab specific commands
+                }
+            }
+        }
+    });
+
+    // Thread for handling serial input
+    std::thread serial_thread([&]() {
+        while (true) {
+            std::string message = controller.sp.readData();
+            if (!message.empty()) {
+                std::lock_guard<std::mutex> lock(controller.command_mutex);
+                if (message == "2\n"  || message == "3\n" || message == "4\n"||message == "5\n"||message == "6\n"||message == "7\n") {
+                    controller.serial_command = message[0];
+                    controller.serial_command_received = true;
+                    controller.handleInput(controller.serial_command);
+                    //handleHomeTabCommand(serial_command,page_num); // Handle HomeTab specific commands
+                    controller.serial_command_received = false;
+                } else {
+                    std::cout << "Received message: " << message << std::endl;
+                }
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        }
+    });
+
+    input_thread.join();
+    serial_thread.join();
 
     return 0;
 }
