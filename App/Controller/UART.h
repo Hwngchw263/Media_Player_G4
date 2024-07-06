5b16983d9ef7a6edf77cfe0d02eaa9528bf7d90a
annhtuan <tuananhbka123@gmail.com>
2024-07-06 14:01:50 +0700

    chua kip them ham moi

--- App/Controller/UART.h
+++ App/Controller/UART.h
@@ -2,14 +2,22 @@
 #define UART_H
 
 #include <string>
-
+#define BUFFER_SIZE 4
 class SerialPort {
 public:
     SerialPort(const char* port);
     ~SerialPort();
     bool configure();
-    std::string readData();
+    std::string receiveData() ;
     static std::string getOpenSDADevicePath();
+    void sendData(const char* data);
+    static void sendfirstdata(const char* data);
+    //ham send o day 
+
+
+    //ham phan tich ban tin
+    //ham tao ban tin 
+    //mcu cung vay 
 
 private:
     int serial_port;
