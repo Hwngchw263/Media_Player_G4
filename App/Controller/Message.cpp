98a4c07f6cf198b651783e8b6c7adaae72b3874a
annhtuan <tuananhbka123@gmail.com>
2024-07-07 22:58:28 +0700

    them fuction convert message to hex string

--- App/Controller/Message.cpp
+++ App/Controller/Message.cpp
@@ -99,4 +99,37 @@ uint32_t DATAMCU::StrtoHex(std::string& ptr, uint8_t len)
 	//return state;
     return pHexa;
 }
+void DATAMCU::uint8ToHexString(uint8_t value, char *str) {
+    const char hexDigits[] = "0123456789ABCDEF";
+    str[0] = hexDigits[(value >> 4) & 0x0F];
+    str[1] = hexDigits[value & 0x0F];
+    str[2] = '\0';
+}
 
+void DATAMCU::uint16toHexString(uint16_t value , char *str){
+	const char hexDigits[] = "0123456789ABCDEF";
+	str[0] = hexDigits[(value >> 12) & 0x0F];
+	str[1] = hexDigits[(value >> 8) & 0x0F];
+	str[2] = hexDigits[(value >> 4) & 0x0F];
+	str[3] = hexDigits[value & 0x0F];
+	str[4] = '\0'; // Null-terminate the string
+}
+
+void DATAMCU::messageToString( char *str) {
+	//01
+    uint8ToHexString((uint8_t)mess.type,&str[0]);
+    //2345
+    uint16toHexString(mess.data, &str[2]);
+    //67
+    uint8ToHexString(mess.checksum, &str[6]);
+    str[8] = '\0';
+}
+
+std::string  DATAMCU::messageToString() {
+    char str[9]; // 2 (type) + 4 (data) + 2 (checksum) + 1 (null-terminator)
+    
+    // Sử dụng sprintf để chuyển đổi tất cả các trường sang hex string
+    sprintf(str, "%02X%04X%02X", mess.type, mess.data, mess.checksum);
+    //du 9 char gom \0
+    return std::string(str); // Trả về chuỗi hex
+}
