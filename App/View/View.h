c6c63284123261f14eb68f940018b32833e811f7
annhtuan <tuananhbka123@gmail.com>
2024-07-10 16:41:22 +0700

    them cac bien choc vao player

--- App/View/View.h
+++ App/View/View.h
@@ -15,9 +15,10 @@ public:
     void printLine(char c, int width);
     void clearScreen();
     void displayPath(std::vector<std::string>& files);
-    void displayMetadata(std::vector<MediaFile>& files);
+    void displayMetadata(std::vector<MediaFile>& files,uint32_t  duration, int cur_song);
     void displayHelp();
     void displayPage(std::vector<std::string>& files);
+    void displaySelection();
     int getpage();
     void setpage(int page);
     Tab gettab();
@@ -28,6 +29,7 @@ private:
     int totalPage = 0;
     static constexpr int ITEMS_PER_PAGE = 5;
     Tab currentTab = HOME;
+    int currsong = 0;
 };
 
 #endif // VIEW_H
